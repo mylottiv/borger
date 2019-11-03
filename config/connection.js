d64bd8b5b910027ff10ab845cf3f85814aa546e7
Thomas Mylott <mylottiv@gmail.com>
2019-11-03 00:01:44 -0500

    more syntax

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 
 // Initialize db connection depending on whether heroku JAWSDB environment or local
-module.exports = (procss.env.JAWSDB_URL) ? mysql.createConnection(process.env.JAWSDB_URL) : 
+module.exports = (process.env.JAWSDB_URL) ? mysql.createConnection(process.env.JAWSDB_URL) : 
     mysql.createConnection({
         host: 'localhost',
         user: 'root',
